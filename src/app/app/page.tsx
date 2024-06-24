7255ac4cb4579bb2cc818d6652596cac82f5680f
Amine Ferkani <74460480+FAMES-CODE@users.noreply.github.com>
2024-06-24 22:50:06 +0200

    modified:   package-lock.json Added React-icons 	modified:   package.json Added React-icons 	modified:   src/app/app/page.tsx New structure 	modified:   src/app/components/app/Sidenav.tsx Added icons

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -24,9 +24,21 @@ export default function Index() {
       <div className="w-5/6 flex flex-col p-8">
         <h1 className="text-3xl font-bold">Summary</h1>
         <div className="grid grid-cols-3 gap-3 mt-8 w-full">
-          <RecapCard title="Total entry" data={10} css={"bg-[#0072f5] text-white font-normal text"} />
-          <RecapCard title="Total of entry" data={10} bg={""} />
-          <RecapCard title="Total of withdraw" data={10} css={"bg-[#17c964] text-white font-normal text"} />
+          <RecapCard
+            title="Total entry"
+            data={10}
+            css={"bg-[#0072f5] text-white shadow-md shadow-[#0072f590] dark:shadow-[#0072f590] drop-shadow-xl font-normal"}
+          />
+          <RecapCard
+            title="Total of entry"
+            data={10}
+            css={" font-normal shadow-md dark:shadow-[#ffffff90] drop-shadow-xl "}
+          />
+          <RecapCard
+            title="Total of withdraw"
+            data={10}
+            css={"bg-[#17c964] shadow-md shadow-[#17c96490] dark:shadow-[#17c96490] text-white font-normal drop-shadow-xl"}
+          />
         </div>
         <div className="">
           <h1 className="text-3xl font-bold my-8">Statistics</h1>
@@ -67,100 +79,87 @@ function DataChart() {
     plugins: {
       legend: {
         position: "bottom" as const,
-      }
+      },
     },
   };
 
   return (
     <Card className="">
       <Line
-      className="p-8"
-      options={options}
-      data={{
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December",
-        ],
-        datasets: [
-          {
-            label: "Entry",
-            data: [65, 59, 80, 81, 56, 55, 40, 30, 20, 10, 5, 10],
-            fill: true,
-            borderColor: "#17c964",
-            backgroundColor: "#17c96420",
-            tension: 0.1,
-          },
-          {
-            label: "Withdraw",
-            data: [12, 19, 3, 5, 2, 3, 10, 11, 12, 19, 3, 5],
-            fill: true,
-            borderColor: "#0072f5",
-            backgroundColor: "#0072f520",
-            tension: 0.1,
-          },
-        ],
-      }}
-    />
+        className="p-8"
+        options={options}
+        data={{
+          labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ],
+          datasets: [
+            {
+              label: "Entry",
+              data: [65, 59, 80, 81, 56, 55, 40, 30, 20, 10, 5, 10],
+              fill: true,
+              borderColor: "#17c964",
+              backgroundColor: "#17c96420",
+              tension: 0.1,
+            },
+            {
+              label: "Withdraw",
+              data: [12, 19, 3, 5, 2, 3, 10, 11, 12, 19, 3, 5],
+              fill: true,
+              borderColor: "#0072f5",
+              backgroundColor: "#0072f520",
+              tension: 0.1,
+            },
+          ],
+        }}
+      />
     </Card>
   );
 }
 
 function RightSide() {
+  const tableRow = [1, 2, 3, 4, 5, 6];
   return (
-    <div className="flex flex-col-reverse justify-between h-full">
-      <Card className="my-4">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <h1 className="font-bold text-large">Latest update</h1>
-        </CardHeader>
-        <CardBody className="overflow-visible py-2">
-          <div>
-            <div className="flex flex-col items-center gap-4">
-              <Card isBlurred shadow="lg">
-                {" "}
-                <CardBody>
-                  <p>
-                    Make beautiful websites regardless of your design
-                    experience.
-                  </p>
+    <div className="h-full">
+      <div>
+        <Card className="">
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <h1 className="font-bold text-large">Latest operations</h1>
+          </CardHeader>
+          {tableRow.map((e, index) => {
+            return (
+              <div key={index}>
+                <CardBody className="overflow-visible py-2">
+                  <div>
+                    <Card>
+                      <div className="flex items-center gap-4">
+                        <User
+                          name="Jane Doe"
+                          avatarProps={{
+                            src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+                          }}
+                        />
+                        <span>4500 USD</span>
+                        <span>10/01/2021</span>
+                      </div>
+                    </Card>
+                  </div>
                 </CardBody>
-              </Card>
-            </div>
-          </div>
-        </CardBody>
-      </Card>
-      <Card className="my-4">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <h1 className="font-bold text-large">Latest operations</h1>
-        </CardHeader>
-        <CardBody className="overflow-visible py-2">
-          <div>
-            <Card>
-              <div className="flex items-center gap-4">
-                <User
-                  name="Jane Doe"
-                  avatarProps={{
-                    src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-                  }}
-                />
-                <span>4500 USD</span>
-                <span>10/01/2021</span>
               </div>
-            </Card>
-          </div>
-        </CardBody>
-        
-        
-      </Card>
+            );
+          })}
+        </Card>
+      </div>
     </div>
   );
 }
--- src/app/components/app/Sidenav.tsx
+++ src/app/components/app/Sidenav.tsx
@@ -1,5 +1,16 @@
 import React from "react";
-import { Avatar, Button } from "@nextui-org/react";
+import { Avatar, Link } from "@nextui-org/react";
+import {
+  FaAlignJustify,
+  FaFileInvoiceDollar,
+  FaFile,
+  FaUsers,
+  FaWarehouse,
+  FaFileLines,
+  FaGear,
+  FaQuestion,
+  FaBook,
+} from "react-icons/fa6";
 
 export default function Sidenav() {
   return (
@@ -27,28 +38,67 @@ function Routes() {
   return (
     <div className="flex flex-col items-start justify-around h-full ml-8">
       <div className="w-full">
-      <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Home</h1>
+        <div className="flex gap-4 items-center hover:bg-[#0072f530] w-3/4 rounded-lg p-2 ">
+          <FaAlignJustify />
+          <h1 className=" ">Home</h1>
+        </div>
         <h2 className="text-gray-400 -ml-4 py-2">Main menu</h2>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Account</h1>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Invoices</h1>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Estimates</h1>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Customers</h1>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Suppliers</h1>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Reports</h1>
+        <Link className="w-full" color="foreground" href="#">
+          <div className="flex gap-4 items-center hover:bg-[#0072f530] w-3/4 rounded-lg p-2 ">
+            <FaFileInvoiceDollar />
+            <h1>Invoices</h1>
+          </div>
+        </Link>
+        <Link className="w-full" color="foreground" href="#">
+          <div className="flex gap-4 items-center hover:bg-[#0072f530] w-3/4 rounded-lg p-2 ">
+            <FaFile />
+            <h1>Estimates</h1>
+          </div>
+        </Link>
+        <Link className="w-full" color="foreground" href="#">
+          <div className="flex gap-4 items-center hover:bg-[#0072f530] w-3/4 rounded-lg p-2 ">
+            <FaUsers />
+            <h1>Customers</h1>
+          </div>
+        </Link>
+        <Link className="w-full" color="foreground" href="#">
+          <div className="flex gap-4 items-center hover:bg-[#0072f530] w-3/4 rounded-lg p-2 ">
+            <FaWarehouse />
+            <h1>Suppliers</h1>
+          </div>
+        </Link>
+        <Link className="w-full" color="foreground" href="#">
+          <div className="flex gap-4 items-center hover:bg-[#0072f530] w-3/4 rounded-lg p-2 ">
+            <FaFileLines />
+            <h1>Reports</h1>
+          </div>
+        </Link>
       </div>
       <div className="w-full">
         <h2 className="text-gray-400 -ml-4 py-2">General</h2>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Settings</h1>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Help</h1>        
+
+        <div className="flex gap-4 items-center hover:bg-[#0072f530] w-3/4 rounded-lg p-2 ">
+          <FaGear />
+          <h1>Settings</h1>
+        </div>
+        <div className="flex gap-4 items-center hover:bg-[#0072f530] w-3/4 rounded-lg p-2 ">
+          <FaQuestion />
+          <h1>Help</h1>
+        </div>
+        <div className="flex gap-4 items-center hover:bg-[#0072f530] w-3/4 rounded-lg p-2 ">
+          <FaBook />
+          <h1>FAQ</h1>
+        </div>
       </div>
       <div className="w-full">
         <h2 className="text-gray-400 -ml-4 py-2">Support</h2>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">FAQ</h1>
+
         <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Support</h1>
         <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Terms</h1>
-        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">Conditions</h1>
+        <h1 className="p-2 rounded-lg hover:bg-[#0072f530] w-3/4">
+          Conditions
+        </h1>
       </div>
     </div>
   );
 }
-
